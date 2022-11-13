6ee6047606c5527d1ff77a520800ca349034f2f6
fizan2904 <fizan.nagarchi6@gmail.com>
2022-11-14 01:45:55 +0530

    login

--- src/schemas/login.js
+++ src/schemas/login.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const joi = require('joi');
 const argon = require('argon2');
+const jwt = require('jsonwebtoken');
+const uuid = require('uuid');
 
 module.exports = async (req, res) => {
     const schema = joi.object({
         email: joi.string().email().required(),
         password: joi.string().min(8).max(24).required(),
+        type: joi.string().valid('cookie', 'bearer').default('cookie'),
     });
 
     const { value, error } = schema.validate(req.body);
@@ -30,18 +33,29 @@ module.exports = async (req, res) => {
         return res.send('Invalid password!');
     }
 
-    const CookieModel = mongoose.model('Cookie');
-    const cookie = await CookieModel.create({
-        userId: user._id,
-        issuedAt: new Date(),
-        expiresAt: new Date(new Date().getTime() + 60 * 60 * 24 * 1000),
+    const token = jwt.sign({
+        email: value.email,
+        avatar: user.avatar,
+    }, 'secret', {
+        expiresIn: '24h',
+        issuer: 'localhost',
+        algorithm: 'HS256',
+        subject: user._id.toString(),
+        jwtid: uuid.v4(),
     });
 
-    res.cookie('auth', cookie._id, {
-        httpOnly: true,
-        domain: 'localhost',
-        maxAge: 24 * 60 * 60 * 1000
-    });
+    if (value.type === 'cookie') {
+        res.cookie('auth', token, {
+            httpOnly: true,
+            domain: 'localhost',
+            maxAge: 24 * 60 * 60 * 1000
+        });
+    
+        return res.send({ success: true });
+    }
 
-    return res.send({ success: true });
+    return res.send({
+        success: true,
+        token: `Bearer ${ token }`,
+    });
 };
