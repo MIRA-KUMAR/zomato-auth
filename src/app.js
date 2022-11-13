3282b5e87100155a3831a9b6ff8705c325e33504
fizan2904 <fizan.nagarchi6@gmail.com>
2022-11-14 01:45:33 +0530

    initiate redis and cors fix

--- src/app.js
+++ src/app.js
@@ -3,13 +3,19 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const IoRedis = require('ioredis');
 
 require('./models');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: (origin, cb) => {
+        cb(null, true)
+    },
+    credentials: true,
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +24,10 @@ app.use(routes);
 
 mongoose.connect('mongodb://localhost:27017/zomato')
     .then(() => {
+        global.redis = new IoRedis({
+            host: 'localhost',
+            port: 6379
+        });
         app.listen(3001, () => {
             console.log('Server running at port 3001');
         });
