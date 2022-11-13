b7a1af1eae8ff190e997cdc5424f1a1da53bf5ac
fizan2904 <fizan.nagarchi6@gmail.com>
2022-11-14 01:45:50 +0530

    verify token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,27 +1,38 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-    if (!req.cookies || !req.cookies.auth) {
-        return res.send('Unauthorized!');
+    try {
+        if ((req.cookies && req.cookies.auth) || req.headers.authorization) {
+            const verifiedToken = jwt.verify(req.cookies.auth || req.headers.authorization.replace('Bearer ', ''), 'secret');
+
+            req.state = {
+                userId: verifiedToken.sub,
+                jti: verifiedToken.jti,
+                exp: verifiedToken.exp
+            };
+        
+            return next();
+        }
+    
+        return res.send({
+            success: false,
+            value: null,
+            error: 'Unauthorized!',
+        });
+    } catch (err) {
+        if (err instanceof jwt.JsonWebTokenError) {
+            return res.send({
+                success: false,
+                value: null,
+                error: 'Unauthorized!',
+            });
+        }
+
+        return res.send({
+            success: false,
+            value: null,
+            error: 'Please try after some time!',
+        });
     }
-
-    const CookieModel = mongoose.model('Cookie');
-    const cookie = await CookieModel.findOne({
-        _id: mongoose.mongo.ObjectId(req.cookies.auth),
-        isDeleted: false
-    });
-
-    if (!cookie) {
-        return res.send('Unauthorized!');
-    }
-
-    if (cookie.expiresAt < new Date()) {
-        return res.send('Unauthorized!');
-    }
-
-    req.state = {
-        userId: cookie.userId,
-    };
-
-    next();
 }
